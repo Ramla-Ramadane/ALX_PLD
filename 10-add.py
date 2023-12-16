96f6b3b942e9cd6530d413e592beab747ebfa228
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-16 10:09:26 +0100

    practicing

---
+++ 10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+print(add(2, 3))
+    
---
+++ unsigned.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*using unsigned int*/
+
+int main()
+{
+	unsigned int i = 12;
+	printf("The value of the unsigned int is , %u\n", i);
+	return (0);
+}
